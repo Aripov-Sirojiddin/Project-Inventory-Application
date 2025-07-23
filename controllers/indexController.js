41e1a64604d695f2f60e55f43ab7688ddfaff27d
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-23 16:33:27 +0500

    Correctly recieving form information and displaying errors in case of wrong inputs from user.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,37 @@
 const db = require("../models/db.js");
 const { validationResult } = require("express-validator");
 
-async function getAllProducts(req, res) {
+async function prepareLocals(req) {
   const products = await db.getAllProducts();
-  res.render("pages/index", {
+  return {
     products: products,
     showForm: req.url === "/new",
-  });
+    errors: [],
+    values: {},
+  };
+}
+
+async function getAllProducts(req, res) {
+  res.render("pages/index", await prepareLocals(req));
 }
 
 async function createProduct(req, res) {
-  req.body
-  res.redirect("/");
+  const productValues = {
+    ...req.body,
+  };
+  const errors = validationResult(req).array();
+  const locals = {
+    ...await prepareLocals(req),
+    errors: errors,
+    values: productValues,
+  };
+  if (errors.length === 0) {
+    console.log(productValues);
+    console.log("Added");
+    res.redirect("/");
+  } else {
+    res.render("pages/index", locals);
+  }
 }
 
 module.exports = {
